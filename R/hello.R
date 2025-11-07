caa4c98d2a29b91a22c6c8d180af2f3d276a247a
Sangston <rms2jg@virginia.edu>
2025-11-07 11:06:04 -0500

    first commit

---
+++ R/hello.R
@@ -0,0 +1,20 @@
+#https://www.youtube.com/watch?v=kQ5QkN4Kx4Q
+
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   https://r-pkgs.org
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+hello <- function() {
+  print("Hello, world!")
+}
