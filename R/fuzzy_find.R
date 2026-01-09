b9bd6501aa6038fc49c40aa2915280679a0a3ff4
Sangston <rms2jg@virginia.edu>
2026-01-09 11:19:17 -0500

    fixed a search and replace error

--- R/fuzzy_find.R
+++ R/fuzzy_find.R
@@ -1,8 +1,8 @@
-fuzzy_find <- function(pattern, string, len_len_moving_window, max_string_distance){
+fuzzy_find <- function(pattern, string, len_moving_window, max_string_distance){
   start_numbers <- c(1:(nchar(string)-(len_moving_window-1)))
   stop_numbers <- start_numbers + (len_moving_window-1)
   v_substr <- Vectorize(substr, vectorize.args = c("start", "stop"))
-  
+
   coordinate_substr <- data.frame("start_coord" = start_numbers,
                                    "end_coord" = stop_numbers,
                                    "substrings" = v_substr(string, start = start_numbers, stop = stop_numbers)
