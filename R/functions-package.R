f1de8cabf24cf8e7a6577fe67ac5c36522d359f4
Sangston <rms2jg@virginia.edu>
2026-06-12 15:11:24 -0400

    More data.table weirdness

---
+++ R/functions-package.R
@@ -0,0 +1,17 @@
+#' functions package
+#'
+#' @import data.table
+#' @keywords internal
+"_PACKAGE"
+
+utils::globalVariables(c(
+  ".N",
+  "h",
+  "i",
+  "j",
+  "N",
+  "seq_id",
+  "n_shared_sketch",
+  "j_est",
+  "jac_kmer"
+))
